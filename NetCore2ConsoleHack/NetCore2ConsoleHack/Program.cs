b766a452dae66a3644b79cd00ff9fa59f90969b0
David Ireland <david@emoose.org>
2018-05-04 17:20:17 +0100

    Project NetCore2ConsoleHack created

---
+++ NetCore2ConsoleHack/NetCore2ConsoleHack/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NetCore2ConsoleHack
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
